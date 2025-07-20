0dff4eaff452075f990af19860b4080c8b7e8954
Christophe Pierrès <79106777+cpierres@users.noreply.github.com>
2025-07-20 22:00:30 +0200

    Merge pull request #1
    * feat: ajout log lors du retour d'une blague aléatoire
    * review : message de log dans `JokeService` traduit en français.

--- back/src/main/java/com/openclassrooms/bobapp/service/JokeService.java
+++ back/src/main/java/com/openclassrooms/bobapp/service/JokeService.java
@@ -4,13 +4,16 @@ import java.util.List;
 import java.util.Random;
 
 import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.openclassrooms.bobapp.data.JsonReader;
 import com.openclassrooms.bobapp.model.Joke;
 
 @Service
 public class JokeService {
-    
+
+    private static final Logger logger = LoggerFactory.getLogger(JokeService.class);
     private final JsonReader jsonReader;
 
     JokeService(JsonReader jsonReader) {
@@ -21,6 +24,7 @@ public class JokeService {
         List<Joke> jokes = this.jsonReader.getJokes();
         Random generator = new Random();
         int randomIndex = generator.nextInt(jokes.size());
+        logger.info("Retourne numéro de la blague: {}", randomIndex);
         return jokes.get(randomIndex);
     }
 }

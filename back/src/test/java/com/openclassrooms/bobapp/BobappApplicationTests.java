979f7523c0fe9025a0cf0d263cabad40949f7488
pierres <c.pierres@larco.fr>
2025-07-31 16:50:17 +0200

    github worflow test back : supprimer le test unitaire en échec avant de faire la PR

--- back/src/test/java/com/openclassrooms/bobapp/BobappApplicationTests.java
+++ back/src/test/java/com/openclassrooms/bobapp/BobappApplicationTests.java
@@ -3,19 +3,10 @@ package com.openclassrooms.bobapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
 @SpringBootTest
 class BobappApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
-	@Test
-	void workflowFailureTest() {
-		// Assertion qui échouera toujours
-		fail("Ce test échoue intentionnellement pour tester le workflow");
-	}
-
 }

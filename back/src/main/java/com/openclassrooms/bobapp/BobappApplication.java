5204b068a87d37ecb6d4c8d91a5068d792ad0335
Christophe Pierrès <79106777+cpierres@users.noreply.github.com>
2025-08-13 20:53:45 +0200

    feat: déclencher Backend CI pour tester synchronisation release-please 20 (#67)

--- back/src/main/java/com/openclassrooms/bobapp/BobappApplication.java
+++ back/src/main/java/com/openclassrooms/bobapp/BobappApplication.java
@@ -10,8 +10,8 @@ public class BobappApplication {
     private static final Logger logger = LoggerFactory.getLogger(BobappApplication.class);
 
     public static void main(String[] args) {
-        logger.info("Starting BobApp Backend... test 19");
-        logger.info("Autre message 19");
+        logger.info("Starting BobApp Backend... test 20");
+        logger.info("Autre message 20");
         logger.info("Encore autre message");
         SpringApplication.run(BobappApplication.class, args);
         logger.info("✅ BobApp Backend démarré avec succès!");

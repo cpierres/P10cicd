60e0419979a49fad24296099278d0a31acc8696b
pierres <c.pierres@larco.fr>
2025-08-14 16:17:07 +0200

    feat(frontend): déclencher Frontend CI pour tester synchronisation release-please avec log feat 2

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent implements OnInit {
   }
 
   public ngOnInit(): void {
-    console.log("log de test FEAT 1 ");
+    console.log("log de test FEAT 2 ");
     this.getRandomJoke();
   }
 
